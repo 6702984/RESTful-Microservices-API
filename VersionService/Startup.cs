73515d4238a06f6542e6b3bfa0425b1671bd369c
Roji <6702984@gmail.com>
2020-02-03 20:37:39 +0800

    Add project. Setup startup.cs, add nuget package for serilog and client side libraries.

---
+++ VersionService/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace VersionService
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseHsts();
+            app.UseStaticFiles();
+            app.UseMvc(options => options.MapRoute("default", "{controller=default}/{action=documentation}"));
+            loggerFactory.AddFile(Configuration.GetSection("SerilogSettings"));
+        }
+    }
+}
